5e5475498c4c56dd525dceb778c5a8b0ef2d5085
scorcaud <samcorcaud@gmail.com>
2022-01-30 19:18:41 +0100

    Updates

---
+++ precache-manifest.f34fad0becc0c6ce15020c345525a37b.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b062fee8d3cf4101fdf4750dc3f7be6a",
+    "url": "/netflix-clone/index.html"
+  },
+  {
+    "revision": "6e416b19e0d18f8eb469",
+    "url": "/netflix-clone/static/css/main.42538a09.chunk.css"
+  },
+  {
+    "revision": "b59b36f93d76836cd9c4",
+    "url": "/netflix-clone/static/js/2.4170fbbe.chunk.js"
+  },
+  {
+    "revision": "6e416b19e0d18f8eb469",
+    "url": "/netflix-clone/static/js/main.a8a40ece.chunk.js"
+  },
+  {
+    "revision": "ded34aa85da1aff6ad44",
+    "url": "/netflix-clone/static/js/runtime~main.21c42e60.js"
+  },
+  {
+    "revision": "adb1364511932b221ab3b31b5a20184b",
+    "url": "/netflix-clone/static/media/abstrait1.adb13645.png"
+  },
+  {
+    "revision": "062a008fe96105a955bf7e1b8c27278b",
+    "url": "/netflix-clone/static/media/abstrait2.062a008f.png"
+  },
+  {
+    "revision": "123adee229914270170bc25b226bcab7",
+    "url": "/netflix-clone/static/media/bob_esponja.123adee2.png"
+  },
+  {
+    "revision": "bb24d0ffbc537b155c4e658ef7d5d972",
+    "url": "/netflix-clone/static/media/happy_tree_friends.bb24d0ff.png"
+  },
+  {
+    "revision": "c99bf7a043161fdd89e1f9376be0b2f8",
+    "url": "/netflix-clone/static/media/la_ligne.c99bf7a0.png"
+  },
+  {
+    "revision": "ef3ae1b581768581c6bc82bd02577e60",
+    "url": "/netflix-clone/static/media/looney_tunes.ef3ae1b5.png"
+  },
+  {
+    "revision": "370a73874cb282314fe671c916dace03",
+    "url": "/netflix-clone/static/media/los_teletubbies.370a7387.png"
+  },
+  {
+    "revision": "5b6294af5413b0611a96b4d126069d57",
+    "url": "/netflix-clone/static/media/mickey.5b6294af.png"
+  },
+  {
+    "revision": "65b3c8de8f6e010003ee71d1660b0876",
+    "url": "/netflix-clone/static/media/peter_griffin.65b3c8de.png"
+  },
+  {
+    "revision": "b665a0b1a6190fef8cb85908bb84ebd5",
+    "url": "/netflix-clone/static/media/picapiedras.b665a0b1.png"
+  },
+  {
+    "revision": "911033daa70d911ba906bbb798a476bb",
+    "url": "/netflix-clone/static/media/pokemon.911033da.png"
+  },
+  {
+    "revision": "b73e7ae755c01bd426d812f45c41820a",
+    "url": "/netflix-clone/static/media/profile.b73e7ae7.png"
+  },
+  {
+    "revision": "8e502de91dc8de985f860add271b3cb3",
+    "url": "/netflix-clone/static/media/raphael.8e502de9.png"
+  },
+  {
+    "revision": "0cfefd4e7cdc503b63dae7123d03abbb",
+    "url": "/netflix-clone/static/media/rickymorty.0cfefd4e.png"
+  },
+  {
+    "revision": "b3b28d805295a9cded028d041cb0874b",
+    "url": "/netflix-clone/static/media/simpson_2_crop.b3b28d80.png"
+  },
+  {
+    "revision": "2eb3c911a0fbac7a93995eca746c3b16",
+    "url": "/netflix-clone/static/media/varios.2eb3c911.png"
+  },
+  {
+    "revision": "773dc5289d1cf74e45f83be96e9103ee",
+    "url": "/netflix-clone/static/media/wallaceGromit.773dc528.png"
+  }
+]);
